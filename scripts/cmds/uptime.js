12a05d50c866da671919badd08eabc2c89eac8ed
md2824066-lgtm <md2824066@gmail.com>
2025-12-01 18:49:35 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -65,7 +65,7 @@ module.exports = {
       ctx.fillText("🤖 BOT UPTIME", 60, 100);
       ctx.fillText(`⏳ ${upTimeStr}`, 60, 200);
       ctx.fillText(`⚡ Ping: ${ping}ms`, 60, 280);
-      ctx.fillText(`👤 Owner: Saimx69x`, 60, 360);
+      ctx.fillText(`👤 Owner: Apon`, 60, 360);
 
       const buffer = canvas.toBuffer("image/png");
       fs.writeFileSync(imagePath, buffer);
@@ -79,7 +79,7 @@ module.exports = {
 ╭─╼━━━━━━━━╾─╮
 │ 💤 Uptime : ${upTimeStr}
 │ ⚡ Ping   : ${ping}ms
-│ 👑 Owner  : Saimx69x
+│ 👑 Owner  : Apon
 ╰─━━━━━━━━━╾─╯
 ━━━━━━━━━━━━━━
         `,
